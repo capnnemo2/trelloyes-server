40a9fe81b4c2de4766d520e88df7a368f8fc5706
Ben Hernandez <benjammin234@gmail.com>
2020-02-20 15:12:12 -0800

    Adds GET endpoints for cards and lists

--- src/app.js
+++ src/app.js
@@ -17,19 +17,78 @@ app.use(cors());
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  transports: [new.winston.File({ filename: info.log })]
+  transports: [new winston.transports.File({ filename: "info.log" })]
 });
 
 if (NODE_ENV !== "production") {
   logger.add(
-    new.winston.transports.Console({
+    new winston.transports.Console({
       format: winston.format.simple()
     })
   );
 }
 
+const cards = [
+  {
+    id: 1,
+    title: "Task One",
+    content: "This is card one"
+  }
+];
+
+const lists = [
+  {
+    id: 1,
+    header: "List One",
+    cardIds: [1]
+  }
+];
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, world");
+});
+
+app.get("/card", (req, res) => {
+  res.json(cards);
+});
+
+app.get("/card/:id", (req, res) => {
+  const { id } = req.params;
+  const card = cards.find(c => c.id == id);
+
+  if (!card) {
+    logger.error(`Card with id ${id} not found`);
+    return res.status(404).send("Card Not Found");
+  }
+
+  res.json(card);
+});
+
+app.get("/list", (req, res) => {
+  res.json(lists);
+});
+
+app.get("/list/:id", (req, res) => {
+  const { id } = req.params;
+  const list = lists.find(li => li.id == id);
+
+  if (!list) {
+    logger.error(`List with id ${id} not found`);
+    return res.status(404).send("List Not Found");
+  }
+
+  res.json(list);
 });
 
 app.use(function errorHandler(error, req, res, next) {
