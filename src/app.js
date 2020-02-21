5e9b1d5fae810d1e3bfb066a143910d325a228ae
Ben Hernandez <benjammin234@gmail.com>
2020-02-20 16:35:18 -0800

    Creates listRouter module

--- src/app.js
+++ src/app.js
@@ -4,9 +4,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-// const winston = require("winston");
-const uuid = require("uuid/v4");
 const cardRouter = require("./card/card-router");
+const listRouter = require("./list/list-router");
 
 const app = express();
 
@@ -16,28 +15,6 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
-// const logger = winston.createLogger({
-//   level: "info",
-//   format: winston.format.json(),
-//   transports: [new winston.transports.File({ filename: "info.log" })]
-// });
-
-// if (NODE_ENV !== "production") {
-//   logger.add(
-//     new winston.transports.Console({
-//       format: winston.format.simple()
-//     })
-//   );
-// }
-
-// const cards = [
-//   {
-//     id: 1,
-//     title: "Task One",
-//     content: "This is card one"
-//   }
-// ];
 
 const lists = [
   {
@@ -59,146 +36,80 @@ app.use(function validateBearerToken(req, res, next) {
 });
 
 app.use(cardRouter);
+app.use(listRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, world");
 });
 
-// app.get("/card", (req, res) => {
-//   res.json(cards);
+// app.get("/list", (req, res) => {
+//   res.json(lists);
 // });
 
-// app.get("/card/:id", (req, res) => {
+// app.get("/list/:id", (req, res) => {
 //   const { id } = req.params;
-//   const card = cards.find(c => c.id == id);
+//   const list = lists.find(li => li.id == id);
 
-//   if (!card) {
-//     logger.error(`Card with id ${id} not found`);
-//     return res.status(404).send("Card Not Found");
+//   if (!list) {
+//     logger.error(`List with id ${id} not found`);
+//     return res.status(404).send("List Not Found");
 //   }
 
-//   res.json(card);
+//   res.json(list);
 // });
 
-app.get("/list", (req, res) => {
-  res.json(lists);
-});
+// app.post("/list", (req, res) => {
+//   const { header, cardIds = [] } = req.body;
 
-app.get("/list/:id", (req, res) => {
-  const { id } = req.params;
-  const list = lists.find(li => li.id == id);
-
-  if (!list) {
-    logger.error(`List with id ${id} not found`);
-    return res.status(404).send("List Not Found");
-  }
-
-  res.json(list);
-});
-
-// app.post("/card", (req, res) => {
-//   const { title, content } = req.body;
-
-//   if (!title) {
-//     logger.error("Title is required");
+//   if (!header) {
+//     logger.error(`Header is required`);
 //     return res.status(400).send("Invalid data");
 //   }
 
-//   if (!content) {
-//     logger.error("Content is required");
-//     return res.status(400).send("Invalid data");
+//   if (cardIds.length > 0) {
+//     let valid = true;
+//     cardIds.forEach(cid => {
+//       const card = cards.find(c => c.id == cid);
+//       if (!card) {
+//         logger.error(`Card with id ${cid} not found in cards array`);
+//         valid = false;
+//       }
+//     });
+//     if (!valid) {
+//       return res.status(400).send("Invalid data");
+//     }
 //   }
 
 //   const id = uuid();
-//   const card = {
+//   const list = {
 //     id,
-//     title,
-//     content
+//     header,
+//     cardIds
 //   };
 
-//   cards.push(card);
+//   lists.push(list);
 
-//   logger.info(`Card with id ${id} created`);
+//   logger.info(`List with id ${id} created`);
 
 //   res
 //     .status(201)
-//     .location(`http://localhost:8000/card/${id}`)
-//     .json(card);
+//     .location(`http://localhost:8000/list/${id}`)
+//     .json(list);
 // });
 
-app.post("/list", (req, res) => {
-  const { header, cardIds = [] } = req.body;
-
-  if (!header) {
-    logger.error(`Header is required`);
-    return res.status(400).send("Invalid data");
-  }
-
-  if (cardIds.length > 0) {
-    let valid = true;
-    cardIds.forEach(cid => {
-      const card = cards.find(c => c.id == cid);
-      if (!card) {
-        logger.error(`Card with id ${cid} not found in cards array`);
-        valid = false;
-      }
-    });
-    if (!valid) {
-      return res.status(400).send("Invalid data");
-    }
-  }
-
-  const id = uuid();
-  const list = {
-    id,
-    header,
-    cardIds
-  };
-
-  lists.push(list);
-
-  logger.info(`List with id ${id} created`);
-
-  res
-    .status(201)
-    .location(`http://localhost:8000/list/${id}`)
-    .json(list);
-});
-
-app.delete("/list/:id", (req, res) => {
-  const { id } = req.params;
-
-  const listIndex = lists.findIndex(li => li.id == id);
-
-  if (listIndex === -1) {
-    logger.error(`List with id ${id} not found`);
-    return res.status(404).send("Not Found");
-  }
-
-  lists.splice(listIndex, 1);
-
-  logger.info(`List with id ${id} deleted`);
-
-  res.status(204).end();
-});
-
-// app.delete("/card/:id", (req, res) => {
+// app.delete("/list/:id", (req, res) => {
 //   const { id } = req.params;
-//   const cardIndex = cards.findIndex(c => c.id == id);
 
-//   if (cardIndex === -1) {
-//     logger.error(`Card with id ${id} not found`);
+//   const listIndex = lists.findIndex(li => li.id == id);
+
+//   if (listIndex === -1) {
+//     logger.error(`List with id ${id} not found`);
 //     return res.status(404).send("Not Found");
 //   }
 
-//   lists.forEach(list => {
-//     const cardIds = list.cardIds.filter(cid => cid !== id);
-//     list.cardIds = cardIds;
-//   });
+//   lists.splice(listIndex, 1);
 
-//   cards.splice(cardIndex, 1);
-
-//   logger.info(`Card with id ${id} deleted`);
+//   logger.info(`List with id ${id} deleted`);
 
 //   res.status(204).end();
 // });
