e7e9659e8179c4a798180504ececdeb5aa1584a6
Ben Hernandez <benjammin234@gmail.com>
2020-02-20 16:50:29 -0800

    Removes generic endpoint

--- src/app.js
+++ src/app.js
@@ -30,10 +30,6 @@ app.use(function validateBearerToken(req, res, next) {
 app.use(cardRouter);
 app.use(listRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello, world");
-});
-
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === "production") {
